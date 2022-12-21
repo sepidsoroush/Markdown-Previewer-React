4bf54c300bf425a0758e76ec640c7cd7dc330c35
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-21 22:13:16 +0200

    create first draft of DOM

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="editor-wrapper">
+        <div className="editor-toolbar">
+          <p>Editor</p>
+          
+        </div>
+        <div id="editor"></div>
+      </div>
+      <div className="preview-wrapper">
+        <div className="preview-toolbar">
+          <p>Previewer</p>
+        </div>
+        <div id="preview"></div>
+      </div>
 
     </div>
   );
