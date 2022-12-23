78c23bb14c335b4c4a2443cd0031c7ff2ca33423
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-23 17:07:33 +0200

    add font family and style to texts of editor and preview

--- src/App.js
+++ src/App.js
@@ -23,7 +23,36 @@ function App() {
           <span>Previewer</span>
           <ArrowsDiagonalMinimize2 />
         </div>
-        <div id="preview"></div>
+        <div id="preview">
+          <p>Welcome to my React Markdown Previewer!
+            This is a sub-heading...
+            And here's some other cool stuff:
+            Heres some code, <div></div>, between 2 backticks.
+
+            // this is multi-line code:
+
+            
+            You can also make text bold... whoa!
+            Or italic.
+            Or... wait for it... both!
+            And feel free to go crazy crossing stuff out.
+
+            There's also links, and
+
+            Block Quotes!
+
+            And if you want to get really crazy, even tables:
+
+            Wild Header	Crazy Header	Another Header?
+            Your content can	be here, and it	can be here....
+            And here.	Okay.	I think we get it.
+            And of course there are lists.
+            Some are bulleted.
+            With different indentation levels.
+            That look like this.
+            And there are numbered lists too.
+            Use just 1s if you want!And last but not least, let's not forget embedded images:</p>
+        </div>
       </div>
 
     </div>
