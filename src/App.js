6ae2ce90d910c58a64902d953830a82494c4e042
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-23 11:20:08 +0200

    import Markdown in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-
+import Markdown from "marked-react";
 import {ArrowsMaximize , ArrowsDiagonalMinimize2} from "tabler-icons-react";
 
 function App() {
+
   return (
     <div className="App">
       <div className="editor-wrapper">
         <div className="editor-toolbar">
           <p>Editor</p>
           <ArrowsMaximize />
-          
         </div>
         <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
       </div>
