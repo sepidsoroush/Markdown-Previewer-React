b50f5b4631f8e74741cbec9f1403269f74de4a0d
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-23 11:12:04 +0200

    Instal Marked

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 
+import {ArrowsMaximize , ArrowsDiagonalMinimize2} from "tabler-icons-react";
+
 function App() {
   return (
     <div className="App">
       <div className="editor-wrapper">
         <div className="editor-toolbar">
           <p>Editor</p>
+          <ArrowsMaximize />
           
         </div>
-        <div id="editor"></div>
+        <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
       </div>
       <div className="preview-wrapper">
         <div className="preview-toolbar">
           <p>Previewer</p>
+          <ArrowsDiagonalMinimize2 />
         </div>
         <div id="preview"></div>
       </div>
