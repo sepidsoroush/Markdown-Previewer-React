a82ed8666bc5ce950395904db4e93ad43aa486c7
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-23 18:42:25 +0200

    apply markdown in function by using useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React, { useState } from 'react';
 import { marked } from "marked";
 import {ArrowsMaximize , ArrowsDiagonalMinimize2} from "tabler-icons-react";
+import './App.css';
 
 function App() {
+  let markdown;
+  const [mark , setMark] = useState('');
 
+  function UpdateMarkdown(e){
+
+    markdown = marked(e.target.value , { sanitize: true });
+    console.log(markdown);
+    setMark( markdown );
+  }
   return (
     <div className="App">
       <div className="editor-wrapper wrapper">
@@ -11,8 +20,7 @@ function App() {
           <span>Editor</span>
           <ArrowsMaximize />
         </div>
-        <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
-        
+        <textarea name="editor" id="editor" cols="30" rows="10" onChange={UpdateMarkdown}></textarea>
       </div>
       <div className="preview-wrapper wrapper">
         <div className="preview-toolbar toolbar">
@@ -20,34 +28,8 @@ function App() {
           <ArrowsDiagonalMinimize2 />
         </div>
         <div id="preview">
-          <p>Welcome to my React Markdown Previewer!
-            This is a sub-heading...
-            And here's some other cool stuff:
-            Heres some code, <div></div>, between 2 backticks.
-
-            // this is multi-line code:
-
-            
-            You can also make text bold... whoa!
-            Or italic.
-            Or... wait for it... both!
-            And feel free to go crazy crossing stuff out.
-
-            There's also links, and
-
-            Block Quotes!
-
-            And if you want to get really crazy, even tables:
-
-            Wild Header	Crazy Header	Another Header?
-            Your content can	be here, and it	can be here....
-            And here.	Okay.	I think we get it.
-            And of course there are lists.
-            Some are bulleted.
-            With different indentation levels.
-            That look like this.
-            And there are numbered lists too.
-            Use just 1s if you want!And last but not least, let's not forget embedded images:</p>
+          {mark}
+          {markdown}
         </div>
       </div>
 
