aa73dbe481920dadf38f3162d6e04564523d0105
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-23 16:46:29 +0200

    Add styles to App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { marked } from "marked";
+import Badge from "react-bootstrap/Badge";
+
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {ArrowsMaximize , ArrowsDiagonalMinimize2} from "tabler-icons-react";
 
@@ -7,16 +10,17 @@ function App() {
 
   return (
     <div className="App">
-      <div className="editor-wrapper">
-        <div className="editor-toolbar">
-          <p>Editor</p>
+      <div className="editor-wrapper wrapper">
+        <div className="editor-toolbar toolbar">
+          <span>Editor</span>
           <ArrowsMaximize />
         </div>
         <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+        
       </div>
-      <div className="preview-wrapper">
-        <div className="preview-toolbar">
-          <p>Previewer</p>
+      <div className="preview-wrapper wrapper">
+        <div className="preview-toolbar toolbar">
+          <span>Previewer</span>
           <ArrowsDiagonalMinimize2 />
         </div>
         <div id="preview"></div>
